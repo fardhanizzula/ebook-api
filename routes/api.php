7ef009346d39da496119b1b04c839590c34ab574
fardhanizzula <frdhnizzl261205@gmail.com>
2022-11-05 19:55:38 +0700

    authentication

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HeloController; 
-use App\Http\Controllers\SiswaContoller; 
-
+use App\Http\Controllers\testController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,17 +19,31 @@ use App\Http\Controllers\SiswaContoller;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-// return $request->user();
-// });
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//   return $request->user();
+//});
 
-route::get('halo', function(){
-    $data = ["me"=> "ganteng"];
+Route::get('halo', function(){
+    $data = ["me" => "Ganteng"];
 
     return $data;
-}) ;
+});
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
+    return $request->user();
+});
 
-route::resource('helocontroller', HeloController::class) ;
-route::resource('siswa', SiswaContoller::class) ;
-route::resource('books', BookController::class) ;
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('testController', testController::class);
+Route::resource('siswa', SiswaController::class);  
+Route::resource('books', BookController::class);  
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit');
+    Route::post('/logout', [AuthController::class, 'logout']) ;
+    Route::resource('authors', AuthorController::class)->except('create', 'edit');
+});
